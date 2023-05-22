ea863e8a85835c1a40fd730fd73dc91bc3ed4fd6
Juan Jesús Campos Garrido <guaje3181@gmail.com>
2023-05-22 13:09:52 +0200

    .

--- scr/SpamFilter/SpamFilter.java
+++ scr/SpamFilter/SpamFilter.java
@@ -28,8 +28,8 @@ public class SpamFilter {
 		final String spamWordsAux = this.spamWords.replaceAll("\\s+", " ").replaceAll(", ", ",");
 		final String[] arrayOfSpamWords = spamWordsAux.toLowerCase().split(",");//Separo las palabras spam
 		final List<String> listOfSpamWords = Arrays.asList(arrayOfSpamWords);//Las meto en un array
-		for (final String word : listOfSpamWords)
-			this.wordsCount += oneSpaceWords.split(word, -1).length - 1;
+		for (final String word : listOfSpamWords)//Recorremos las palabras
+			this.wordsCount += oneSpaceWords.split(word, -1).length - 1;// Partimos la linea string utilizando como caracter una palbra de spam y contamos los trozos;
 		final double umbral = (double) this.wordsCount / listOfImputWords.size();
 		return umbral > this.threshold;//Si umbral es mayor que threshold Tenemos Spam
 	}
